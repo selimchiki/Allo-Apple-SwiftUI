458b0277dcd09391a2fb566611cffbbb4a8feee9
Selim Chiki <selimchiki@yahoo.fr>
2019-10-23 16:16:02 +0200

    add function for different request for api

--- Allo-Apple-SwiftUI/Controller/MenuController.swift
+++ Allo-Apple-SwiftUI/Controller/MenuController.swift
@@ -12,5 +12,16 @@ class MenuController {
     
     let baseURL = URL(string: "http://localhost:8090/")!
     
+    func fetchCategories(completion: @escaping ([String]?) -> Void) {
+        
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
+        
+    }
+    
+    func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
+        
+    }
     
 }
