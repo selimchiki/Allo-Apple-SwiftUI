e7952992214483e0f8c4442af69bd77c5c9df5cb
Selim Chiki <selimchiki@yahoo.fr>
2019-10-23 16:13:01 +0200

    Create Class MenuController
    Create MenuController for network code

---
+++ Allo-Apple-SwiftUI/Controller/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  Allo-Apple-SwiftUI
+//
+//  Created by Sélim Chiki on 23/10/2019.
+//  Copyright © 2019 Sélim Chiki. All rights reserved.
+//
+
+import Foundation
+
+class MenuController {
+    
+    let baseURL = URL(string: "http://localhost:8090/")!
+    
+    
+}
