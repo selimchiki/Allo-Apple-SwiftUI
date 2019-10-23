8b3ae47496341fe4d754478ca347174bca513972
Selim Chiki <selimchiki@yahoo.fr>
2019-10-23 16:27:37 +0200

    add code in differents function for network

--- Allo-Apple-SwiftUI/Controller/MenuController.swift
+++ Allo-Apple-SwiftUI/Controller/MenuController.swift
@@ -10,18 +10,63 @@ import Foundation
 
 class MenuController {
     
+    static var shared = MenuController()
+    
     let baseURL = URL(string: "http://localhost:8090/")!
     
     func fetchCategories(completion: @escaping ([String]?) -> Void) {
         
+        let categoryURL = baseURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoryURL) { (data, response, error) in
+            if let data = data, let jsonDictionnary = try? JSONSerialization.jsonObject(with: data) as? [String: Any], let categories = jsonDictionnary["categories"] as? [String] {
+                completion(categories)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
     }
     
     func fetchMenuItems(forCategory categoryName: String, completion: @escaping ([MenuItem]?) -> Void) {
         
+        let initialMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "menu", value: categoryName)]
+        let menuURL = components.url!
+        
+        let task = URLSession.shared.dataTask(with: menuURL) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data, let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) {
+                completion(menuItems.items)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
     }
     
     func submitOrder(forMenuIDs menuIds: [Int], completion: @escaping (Int?) -> Void) {
         
+        let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let data: [String: [Int]] = ["menuIds": menuIds]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            
+            if let data = data, let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) {
+                completion(preparationTime.prepTime)
+            } else {
+                completion(nil)
+            }
+        }
+        task.resume()
     }
     
 }
---
+++ Allo-Apple-SwiftUI/View/CategoryTableView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryTableView.swift
+//  Allo-Apple-SwiftUI
+//
+//  Created by Sélim Chiki on 23/10/2019.
+//  Copyright © 2019 Sélim Chiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryTableView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryTableView()
+    }
+}
